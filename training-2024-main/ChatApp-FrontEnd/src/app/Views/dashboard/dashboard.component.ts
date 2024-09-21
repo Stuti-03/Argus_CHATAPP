31fddbb2501dd108891b3e372d30f087caa1b754
Stuti-03 <stuti.mittal@argusoft.in>
2024-09-21 10:54:24 +0530

    dashboard frontend update

--- training-2024-main/ChatApp-FrontEnd/src/app/Views/dashboard/dashboard.component.ts
+++ training-2024-main/ChatApp-FrontEnd/src/app/Views/dashboard/dashboard.component.ts
@@ -30,24 +30,10 @@ export class DashboardComponent implements OnInit{
       error: (error: any) => {
         console.log(error);
       }
-    });
-
-  // conversations = [
-  //   {name: "David", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false, },
-  //   {name: "James", time:"8:21", latestMessage: "Good Morning!" ,  latestMessageRead: true},
-  //   {name: "Andrew", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false},
-  //   {name: "Richard", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: true},
-  //   {name: "Dyno", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false},
-  //   {name: "Julie", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false},
-  //   {name: "Tom", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: true},
-  //   {name: "Jerry", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false},
-  //   {name: "Grey", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false},
-  //   {name: "Jill", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: true},
-  //   {name: "Blue", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false},
-  //   {name: "King", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: false},
-  //   {name: "Kong", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: true},
-  //   {name: "Rock", time:"8:21", latestMessage: "Good Morning!" , latestMessageRead: true},
-
-  // ]
-
-}}
+    })
+  }
+    selectedUser: User | null = null;
+    selectUser(users: User) : void{
+      this.selectedUser = users;
+    }
+  }
